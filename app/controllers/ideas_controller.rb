c7d27ffbc092ad2e05fb1c07912ac636b3c0571f
Michael Dao <mike@turing.edu>
2023-05-16 18:05:19 -0600

    turbo working

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -5,7 +5,19 @@ class IdeasController < ApplicationController
 
   def create
     Idea.create(idea_params)
-    redirect_to root_path
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          :idea_index,
+          partial: 'layouts/idea_index',
+          locals: { ideas: Idea.all }
+        )
+      end
+      format.html do 
+        redirect_to root_path 
+      end
+    end
   end
 
   def idea_params
